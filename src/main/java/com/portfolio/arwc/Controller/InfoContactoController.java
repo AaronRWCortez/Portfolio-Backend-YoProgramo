68257609016c5ccb4b089306d3f9d0855100f7be
Aaron Ramiro Waldemar Cortez <99484508+AaronRWCortez@users.noreply.github.com>
2022-08-06 01:46:06 -0300

    probando conexion firebase y clevercloud

--- src/main/java/com/portfolio/arwc/Controller/InfoContactoController.java
+++ src/main/java/com/portfolio/arwc/Controller/InfoContactoController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin (origins = "http://localhost:4200")
+@CrossOrigin (origins = "https://frontend-arwc.web.app")
 public class InfoContactoController {
         @Autowired IInfoContactoService iInfoContactoService;
     
--- src/main/java/com/portfolio/arwc/Controller/RedSocialController.java
+++ src/main/java/com/portfolio/arwc/Controller/RedSocialController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin (origins = "http://localhost:4200")
+@CrossOrigin (origins = "https://frontend-arwc.web.app")
 public class RedSocialController {
     @Autowired IRedSocialService iRedSocialService;
     
