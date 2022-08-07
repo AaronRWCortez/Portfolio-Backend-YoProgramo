ad14c0197eb52bd4e58680eadabd4abdb5abb639
Aaron Ramiro Waldemar Cortez <99484508+AaronRWCortez@users.noreply.github.com>
2022-08-07 02:18:58 -0300

    i

--- src/main/java/com/portfolio/arwc/Controller/ExperienciaController.java
+++ src/main/java/com/portfolio/arwc/Controller/ExperienciaController.java
@@ -5,6 +5,7 @@ import com.portfolio.arwc.Interface.IExperienciaService;
 import java.util.List;
 import javax.annotation.security.RolesAllowed;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin (origins = "https://frontend-arwc.web.app")
 public class ExperienciaController {
 
     @Autowired
