66c942c347f03090cd28181cd9d0c46bc72ac164
Aaron Ramiro Waldemar Cortez <99484508+AaronRWCortez@users.noreply.github.com>
2022-08-06 22:50:14 -0300

    prueba

--- src/main/java/com/portfolio/arwc/Controller/EducacionController.java
+++ src/main/java/com/portfolio/arwc/Controller/EducacionController.java
@@ -5,49 +5,53 @@ import com.portfolio.arwc.Interface.IEducacionService;
 import java.util.List;
 import javax.annotation.security.RolesAllowed;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("educaciones")
+@CrossOrigin (origins = "https://frontend-arwc.web.app")
 public class EducacionController {
     @Autowired IEducacionService iEducacionService;
     
-    @GetMapping("/educaciones/traer")
+    @GetMapping("/traer")
     public List<Educacion> getEducacion(){
         return iEducacionService.getEducacion();
     }
     
-    @GetMapping("/educaciones/traer/{id}")
+    @GetMapping("/traer/{id}")
     public Educacion getEducacionID(@PathVariable Long id){
         return iEducacionService.findEducacion(id);   
     }
     
-    @GetMapping("/educaciones/traer/persona/{id}")
+    @GetMapping("/traer/persona/{id}")
     public List<Educacion> getEducacionByUser(@PathVariable Long id) {
         return iEducacionService.getEducacionByPersonaID(id);
     }
     
     @RolesAllowed({"ROLE_ADMIN"})
-    @PostMapping("/educaciones/crear")
+    @PostMapping("/crear")
     public Educacion createEducacion(@RequestBody Educacion educacion){
         iEducacionService.saveEducacion(educacion);
         return educacion;
     }
     
     @RolesAllowed({"ROLE_ADMIN"})
-    @DeleteMapping("/educaciones/borrar/{id}")
+    @DeleteMapping("/borrar/{id}")
     public Long deleteEducacion(@PathVariable Long id){
         iEducacionService.deleteEducacion(id);
         return id;
     }
     
     @RolesAllowed({"ROLE_ADMIN"})
-    @PutMapping("/educaciones/editar/{id}")
+    @PutMapping("/editar/{id}")
     public Educacion editEducacion(
             @PathVariable Long id, @RequestBody Educacion p){
         Educacion educacion = iEducacionService.findEducacion(id);
